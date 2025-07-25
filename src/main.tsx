6502ef73fa8dc481af8a9b3b1eabc23c30a1ec5d
drnirav83 <drniravpandya@gmail.com>
2025-07-25 18:27:42 +0530

    Replace App.tsx with base layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Route, Switch } from "wouter";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
+// src/App.tsx
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
+import { Header } from './components/Header'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+function App() {
+  return (
     <QueryClientProvider client={queryClient}>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/upload" component={Upload} />
-      </Switch>
+      <Header />
     </QueryClientProvider>
-  </React.StrictMode>
-);
+  )
+}
+
+export default App
