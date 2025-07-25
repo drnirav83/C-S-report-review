883e36ac729bc6c1c9601ec4654a06348241273e
drnirav83 <drniravpandya@gmail.com>
2025-07-25 18:46:12 +0530

    Delete src/src/components directory

--- src/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-/components/Header.tsx
-export function Header() {
-  return (
-    <header>
-      <h1>Microcheck Report Aid</h1>
-      <p>Validate microbiology culture & sensitivity reports</p>
-    </header>
-  )
-}
