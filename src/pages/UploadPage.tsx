b1ca033efb50730ebbaaf1e86b1d228a7d0efcdd
drnirav83 <drniravpandya@gmail.com>
2025-07-25 19:22:43 +0530

    Create UploadPage.tsx

---
+++ src/pages/UploadPage.tsx
@@ -0,0 +1,9 @@
+// src/pages/UploadPage.tsx
+export default function UploadPage() {
+  return (
+    <div>
+      <h2>Upload Report</h2>
+      <p>This page will allow uploading AST reports for validation.</p>
+    </div>
+  );
+}
