4007318bf485212f98e67478fb440a175d288618
drnirav83 <drniravpandya@gmail.com>
2025-07-25 18:46:49 +0530

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+// src/components/Header.tsx
+export function Header() {
+  return (
+    <header>
+      <h1>Microcheck Report Aid</h1>
+      <p>Validate microbiology culture & sensitivity reports</p>
+    </header>
+  )
+}
