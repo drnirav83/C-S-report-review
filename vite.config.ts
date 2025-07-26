a67f6c5d1ddfd06f6f1b5570daff74b2978b9670
drnirav83 <drniravpandya@gmail.com>
2025-07-26 19:21:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
-});
+    port: 3001,
+    // Enable SPA fallback for React Router
+    historyApiFallback: true,
+  },
+})
