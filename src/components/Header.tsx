9f56bbccef49bf5d9ab16ceb56022c0a6e0bf852
drnirav83 <drniravpandya@gmail.com>
2025-07-26 18:22:00 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
 // src/components/Header.tsx
+import { Link } from 'react-router-dom'
+import './Header.css'
+
 export function Header() {
   return (
     <header>
       <h1>Microcheck Report Aid</h1>
       <p>Validate microbiology culture & sensitivity reports</p>
+      <nav>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/upload">Upload</Link> |{' '}
+        <Link to="/review">Review</Link> |{' '}
+        <Link to="/correction">Correction</Link>
+      </nav>
     </header>
   )
 }
